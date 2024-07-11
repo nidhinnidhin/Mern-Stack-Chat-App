d61942ab66fdf3b75ab89d6de81c90356703d7c4
nidhinidhin <nidhinbabu171@gmail.com>
2024-07-11 23:34:59 +0530

    currentt

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import PORT from '../config'
 
-// const baseURL = `https://mern-stack-chat-app-1.onrender.com`;
-const baseURL = `https://localhost:5000`;
+const baseURL = `https://mern-stack-chat-app-1.onrender.com`;
 
 export const fetchChats = async () => {
     try{
