5922df7a972928c74185879d13d21109d51d380c
nidhinidhin <nidhinbabu171@gmail.com>
2024-07-19 19:47:00 +0530

    latest update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,28 +1,3 @@
-import axios from 'axios';
-
-let token = '';
-
-if (localStorage.getItem("userInfo")) {
-    const userInfoString = localStorage.getItem("userInfo");
-    const userInfo = JSON.parse(userInfoString);
-    token = userInfo.token;
-    console.log("token", token);
-}
-
-const PORT = process.env.REACT_APP_BACKEND_PORT;
-
-const axiosInstance = axios.create({
-    baseURL: `https://mern-stack-chat-app-wu4f.onrender.com`,
-    headers: {'Content-Type': 'application/json'},
-});
-
-export const axiosReqWithToken = axios.create({
-    baseURL: `https://mern-stack-chat-app-wu4f.onrender.com`,
-    headers: { Authorization: `Bearer ${token}` }
-});
-
-export default axiosInstance;
-
 // import axios from 'axios';
 
 // let token = '';
@@ -37,13 +12,38 @@ export default axiosInstance;
 // const PORT = process.env.REACT_APP_BACKEND_PORT;
 
 // const axiosInstance = axios.create({
-//     baseURL: `http://127.0.0.1:${PORT}`,
+//     baseURL: `https://mern-stack-chat-app-wu4f.onrender.com`,
 //     headers: {'Content-Type': 'application/json'},
 // });
 
 // export const axiosReqWithToken = axios.create({
-//     baseURL: `http://127.0.0.1:${PORT}`,
+//     baseURL: `https://mern-stack-chat-app-wu4f.onrender.com`,
 //     headers: { Authorization: `Bearer ${token}` }
 // });
 
 // export default axiosInstance;
+
+import axios from 'axios';
+
+let token = '';
+
+if (localStorage.getItem("userInfo")) {
+    const userInfoString = localStorage.getItem("userInfo");
+    const userInfo = JSON.parse(userInfoString);
+    token = userInfo.token;
+    console.log("token", token);
+}
+
+const PORT = process.env.REACT_APP_BACKEND_PORT;
+
+const axiosInstance = axios.create({
+    baseURL: `http://127.0.0.1:${PORT}`,
+    headers: {'Content-Type': 'application/json'},
+});
+
+export const axiosReqWithToken = axios.create({
+    baseURL: `http://127.0.0.1:${PORT}`,
+    headers: { Authorization: `Bearer ${token}` }
+});
+
+export default axiosInstance;
